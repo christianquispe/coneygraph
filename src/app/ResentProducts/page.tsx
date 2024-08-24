26be7d0c497e44258d1635f4344377d2d924f8a5
johan Quispe C <deimonsu@gmai.com>
2024-08-23 22:39:55 -0500

    feat: modify resentProducts section button

--- src/app/ResentProducts/page.tsx
+++ src/app/ResentProducts/page.tsx
@@ -4,12 +4,15 @@ import Text from "@/components/Text";
 import Button from "@/components/Button";
 import {jobsData} from "../../components/JobsDone";
 import Image from "next/image";
+import styles from "./styles.module.css"
 
 export default function ResentProducts({ params }: any) {
 
  return (
   <Container>
    <section>
+    <Title level="1" className="text-center pb-10">CATALOGO</Title>
+    <div className={styles.productsContainer}>
       {jobsData.map((item, index) => (
               <div key={index} >
                 <a href={item.link}>
@@ -24,6 +27,7 @@ export default function ResentProducts({ params }: any) {
                 <a href={item.link}>{item.title}</a>
               </div>
             ))}
+    </div>
    </section>
   </Container>
  );
